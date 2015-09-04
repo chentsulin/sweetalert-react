acde36771b5547d8cc177fe422b45c98a8ccbff4
C.T. Lin <chentsulin@gmail.com>
2015-09-04 16:55:05 +0800

    fix ignore dist

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -13,7 +13,7 @@ module.exports = {
     ]
   },
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'static'),
     filename: 'bundle.js',
     publicPath: '/'
   },
