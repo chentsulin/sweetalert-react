298573efded1c090141006b9c0242931a454a120
Andrew Lisowski <hipstersmoothie@users.noreply.github.com>
2018-03-08 00:40:30 -0800

    Function that accepts swal instance and returns the component (#6)
    * default funciton accepts swal instance and returns the component
    
    * don't mess with default export
    
    * should default to swal provided in jsx if one isn't specified in withSwal
    
    * bind the argument in a way that will work
    
    * change name to asReactComponent
    
    * unused variable
    
    * still allow swal to be overriden
    
    * move to function that returns a class

--- src/SweetAlert.js
+++ src/SweetAlert.js
@@ -73,191 +73,195 @@ function warningRemoved(props) {
   })
 }
 
-export default class SweetAlert extends Component {
-  /* eslint-disable react/no-unused-prop-types */
-  static propTypes = {
-    // sweetalert option
-    title: PropTypes.string.isRequired,
-    text: PropTypes.string,
-    swal: PropTypes.func,
-    type: PropTypes.oneOf(['warning', 'error', 'success', 'info', 'input']),
-    customClass: PropTypes.string,
-    showCancelButton: PropTypes.bool,
-    showConfirmButton: PropTypes.bool,
-    confirmButtonText: PropTypes.string,
-    confirmButtonColor: PropTypes.string,
-    confirmButtonClass: PropTypes.string,
-    cancelButtonText: PropTypes.string,
-    cancelButtonClass: PropTypes.string,
-    reverseButtons: PropTypes.bool,
-    buttonsStyling: PropTypes.bool,
-    imageUrl: PropTypes.string,
-    html: PropTypes.bool,
-    animation: PropTypes.oneOfType([
-      PropTypes.bool,
-      PropTypes.oneOf(['pop', 'slide-from-top', 'slide-from-bottom']),
-    ]),
-    // inputType: PropTypes.oneOf(ALLOWS_INPUT_TYPES),
-    inputPlaceholder: PropTypes.string,
-    inputValue: PropTypes.string,
-    showLoaderOnConfirm: PropTypes.bool,
+export const asReactComponent = swalInstance => (
+  class SweetAlert extends Component {
+    /* eslint-disable react/no-unused-prop-types */
+    static propTypes = {
+      // sweetalert option
+      title: PropTypes.string.isRequired,
+      text: PropTypes.string,
+      type: PropTypes.oneOf(['warning', 'error', 'success', 'info', 'input']),
+      customClass: PropTypes.string,
+      showCancelButton: PropTypes.bool,
+      showConfirmButton: PropTypes.bool,
+      confirmButtonText: PropTypes.string,
+      confirmButtonColor: PropTypes.string,
+      confirmButtonClass: PropTypes.string,
+      cancelButtonText: PropTypes.string,
+      cancelButtonClass: PropTypes.string,
+      reverseButtons: PropTypes.bool,
+      buttonsStyling: PropTypes.bool,
+      imageUrl: PropTypes.string,
+      html: PropTypes.bool,
+      animation: PropTypes.oneOfType([
+        PropTypes.bool,
+        PropTypes.oneOf(['pop', 'slide-from-top', 'slide-from-bottom']),
+      ]),
+      // inputType: PropTypes.oneOf(ALLOWS_INPUT_TYPES),
+      inputPlaceholder: PropTypes.string,
+      inputValue: PropTypes.string,
+      showLoaderOnConfirm: PropTypes.bool,
 
-    // custom option
-    show: PropTypes.bool,
-    onConfirm: PropTypes.func,
-    onCancel: PropTypes.func,
-    onClose: PropTypes.func,
-    onEscapeKey: PropTypes.func,
-    onOutsideClick: PropTypes.func,
-  }
-  /* eslint-enable react/no-unused-prop-types */
-
-  static defaultProps = {
-    // sweetalert option
-    text: null,
-    type: null,
-    customClass: null,
-    showCancelButton: false,
-    showConfirmButton: true,
-    confirmButtonText: 'OK',
-    confirmButtonColor: '#aedef4',
-    cancelButtonText: 'Cancel',
-    cancelButtonClass: null,
-    confirmButtonClass: null,
-    buttonsStyling: true,
-    reverseButtons: false,
-    imageUrl: null,
-    html: false,
-    animation: true,
-    // inputType: 'text',
-    inputPlaceholder: null,
-    inputValue: null,
-    showLoaderOnConfirm: false,
-
-    // custom option
-    show: false,
-  }
-
-  constructor(props, context) {
-    super(props, context)
-
-    this._show = false
-    this.swal = props.swal || swal
-  }
-
-  componentDidMount() {
-    this.setupWithProps(this.props)
-
-    if (this.props.onOutsideClick) {
-      this.registerOutsideClickHandler(this.props.onOutsideClick)
+      // custom option
+      show: PropTypes.bool,
+      onConfirm: PropTypes.func,
+      onCancel: PropTypes.func,
+      onClose: PropTypes.func,
+      onEscapeKey: PropTypes.func,
+      onOutsideClick: PropTypes.func,
     }
-  }
+    /* eslint-enable react/no-unused-prop-types */
 
-  componentWillReceiveProps(props) {
-    this.setupWithProps(props)
+    static defaultProps = {
+      // sweetalert option
+      text: null,
+      type: null,
+      customClass: null,
+      showCancelButton: false,
+      showConfirmButton: true,
+      confirmButtonText: 'OK',
+      confirmButtonColor: '#aedef4',
+      cancelButtonText: 'Cancel',
+      cancelButtonClass: null,
+      confirmButtonClass: null,
+      buttonsStyling: true,
+      reverseButtons: false,
+      imageUrl: null,
+      html: false,
+      animation: true,
+      // inputType: 'text',
+      inputPlaceholder: null,
+      inputValue: null,
+      showLoaderOnConfirm: false,
 
-    const oldOutsideClickHandler = this.props.onOutsideClick
-    const newOutsideClickHandler = props.onOutsideClick
+      // custom option
+      show: false,
+    }
 
-    if (oldOutsideClickHandler !== newOutsideClickHandler) {
-      if (oldOutsideClickHandler && newOutsideClickHandler) {
-        this.unregisterOutsideClickHandler()
-        this.registerOutsideClickHandler(newOutsideClickHandler)
-      } else if (oldOutsideClickHandler && !newOutsideClickHandler) {
-        this.unregisterOutsideClickHandler()
-      } else if (!oldOutsideClickHandler && newOutsideClickHandler) {
-        this.registerOutsideClickHandler(newOutsideClickHandler)
+    constructor(props, context) {
+      super(props, context)
+
+      this._show = false
+      this._swal = swalInstance
+    }
+
+    componentDidMount() {
+      this.setupWithProps(this.props)
+
+      if (this.props.onOutsideClick) {
+        this.registerOutsideClickHandler(this.props.onOutsideClick)
       }
     }
-  }
 
-  componentWillUnmount() {
-    this.unregisterOutsideClickHandler()
-    this.unbindEscapeKey()
-  }
+    componentWillReceiveProps(props) {
+      this.setupWithProps(props)
 
-  setupWithProps(props) {
-    warningRemoved(props)
-    const { show, onConfirm, onCancel, onClose, onEscapeKey } = props
-    if (show) {
-      this.swal(
-        {
-          ...pick(props, ALLOWS_KEYS),
-          ...OVERWRITE_PROPS,
-        }).then(
-          () => {
-            this.handleClickConfirm(onConfirm)
-          },
-          (dismiss) => {
-            this.handleClickCancel(onCancel, dismiss)
-          },
-        )
-      this._show = true
-      if (onEscapeKey) this.bindEscapeKey(onEscapeKey)
-    } else {
-      this.handleClose(onClose)
+      const oldOutsideClickHandler = this.props.onOutsideClick
+      const newOutsideClickHandler = props.onOutsideClick
+
+      if (oldOutsideClickHandler !== newOutsideClickHandler) {
+        if (oldOutsideClickHandler && newOutsideClickHandler) {
+          this.unregisterOutsideClickHandler()
+          this.registerOutsideClickHandler(newOutsideClickHandler)
+        } else if (oldOutsideClickHandler && !newOutsideClickHandler) {
+          this.unregisterOutsideClickHandler()
+        } else if (!oldOutsideClickHandler && newOutsideClickHandler) {
+          this.registerOutsideClickHandler(newOutsideClickHandler)
+        }
+      }
     }
-  }
 
-  registerOutsideClickHandler(handler) {
-    this._outsideClickHandler = outsideTargetHandlerFactory(
-      document.getElementsByClassName('sweet-alert')[0],
-      handler,
-    )
-    this.enableOutsideClick()
-  }
-
-  unregisterOutsideClickHandler() {
-    this.disableOutsideClick()
-    this._outsideClickHandler = null
-  }
-
-  enableOutsideClick() {
-    const fn = this._outsideClickHandler
-    if (fn) {
-      document.addEventListener('mousedown', fn)
-      document.addEventListener('touchstart', fn)
-    }
-  }
-
-  disableOutsideClick() {
-    const fn = this._outsideClickHandler
-    if (fn) {
-      document.removeEventListener('mousedown', fn)
-      document.removeEventListener('touchstart', fn)
-    }
-  }
-
-  bindEscapeKey(onEscapeKey) {
-    mousetrap.bind('esc', onEscapeKey)
-  }
-
-  unbindEscapeKey() {
-    mousetrap.unbind('esc')
-  }
-
-  handleClickConfirm(onConfirm) {
-    if (onConfirm) {
-      onConfirm()
-    }
-  }
-
-  handleClickCancel(onCancel) {
-    if (onCancel) {
-      onCancel()
-    }
-  }
-
-  handleClose(onClose) {
-    if (this._show) {
-      this.swal.close()
+    componentWillUnmount() {
+      this.unregisterOutsideClickHandler()
       this.unbindEscapeKey()
-      if (onClose) onClose()
-      this._show = false
+    }
+
+    setupWithProps(props) {
+      warningRemoved(props)
+      const { show, onConfirm, onCancel, onClose, onEscapeKey } = props
+      if (show) {
+        this._swal(
+          {
+            ...pick(props, ALLOWS_KEYS),
+            ...OVERWRITE_PROPS,
+          }).then(
+            () => {
+              this.handleClickConfirm(onConfirm)
+            },
+            (dismiss) => {
+              this.handleClickCancel(onCancel, dismiss)
+            },
+        )
+        this._show = true
+        if (onEscapeKey) this.bindEscapeKey(onEscapeKey)
+      } else {
+        this.handleClose(onClose)
+      }
+    }
+
+    registerOutsideClickHandler(handler) {
+      this._outsideClickHandler = outsideTargetHandlerFactory(
+        document.getElementsByClassName('sweet-alert')[0],
+        handler,
+      )
+      this.enableOutsideClick()
+    }
+
+    unregisterOutsideClickHandler() {
+      this.disableOutsideClick()
+      this._outsideClickHandler = null
+    }
+
+    enableOutsideClick() {
+      const fn = this._outsideClickHandler
+      if (fn) {
+        document.addEventListener('mousedown', fn)
+        document.addEventListener('touchstart', fn)
+      }
+    }
+
+    disableOutsideClick() {
+      const fn = this._outsideClickHandler
+      if (fn) {
+        document.removeEventListener('mousedown', fn)
+        document.removeEventListener('touchstart', fn)
+      }
+    }
+
+    bindEscapeKey(onEscapeKey) {
+      mousetrap.bind('esc', onEscapeKey)
+    }
+
+    unbindEscapeKey() {
+      mousetrap.unbind('esc')
+    }
+
+    handleClickConfirm(onConfirm) {
+      if (onConfirm) {
+        onConfirm()
+      }
+    }
+
+    handleClickCancel(onCancel) {
+      if (onCancel) {
+        onCancel()
+      }
+    }
+
+    handleClose(onClose) {
+      if (this._show) {
+        this._swal.close()
+        this.unbindEscapeKey()
+        if (onClose) onClose()
+        this._show = false
+      }
+    }
+
+    render() {
+      return null
     }
   }
+)
 
-  render() {
-    return null
-  }
-}
+export default asReactComponent(swal)
+
